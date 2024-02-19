0a0fb184f1b34c17e31aaacec40fcd900d85fc06
carine-69 <umugabecarine@gmail.com>
2024-02-19 19:39:43 +0200

    student records file

--- main.sh
+++ main.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-#
+# script to create a student record
+ read -p 'Enter Email:' email
+ read -p 'Enter student Age: age
+ read -p 'Enter  student ID' student-id
+ # script to save student record
+ echo '$email,$age,$student-id' >>student-list_1023.txt
